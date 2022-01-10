d3048085c45a3608968bc9f0935097727fc99050
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2022-01-10 15:53:49 +0100

    build

---
+++ client/build/precache-manifest.25c7364d050872cc0ce6d7bdf649096e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c85b9f13d0c01d9a869c87794e1b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe3921df05670858b1e8",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "68a9d5217bb9cfbe1a9f",
+    "url": "/static/js/2.076c4db7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.076c4db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3921df05670858b1e8",
+    "url": "/static/js/main.b546d491.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
